042fc1e0262358bc5cb86eeded95b2af205e5889
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-04-12 19:27:05 +0200

    add animation to splash screen

--- bookly/lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ bookly/lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,25 +1,48 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({
     super.key,
   });
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> fadeAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 2500));
+    fadeAnimation = Tween<double>(begin: 0, end: 1).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset(AssetsData.logo),
-        const SizedBox(height: 4),
-        Text(
-          "Read Free Books",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-        ),
-      ],
+    return AnimatedBuilder(
+      animation: fadeAnimation,
+      builder: (context, _) {
+        return FadeTransition(
+          opacity: fadeAnimation,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset(AssetsData.logo),
+              const SizedBox(height: 4),
+              Text(
+                "Read Free Books",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
