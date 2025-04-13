b7602e1827342c3303ec3a1136cc7fbaf4228e31
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-04-13 18:37:48 +0200

    add custom App Bar to home

---
+++ bookly/lib/features/splash/presentation/views/widgets/fade_animation_body.dart
@@ -0,0 +1,58 @@
+import 'package:bookly/constants.dart';
+import 'package:bookly/features/home/presentation/views/home_view.dart';
+import 'package:bookly/features/splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class FadeAnimaionBody extends StatefulWidget {
+  const FadeAnimaionBody({
+    super.key,
+  });
+
+  @override
+  State<FadeAnimaionBody> createState() => _FadeAnimaionBodyState();
+}
+
+class _FadeAnimaionBodyState extends State<FadeAnimaionBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> fadeAnimation;
+  @override
+  void initState() {
+    super.initState();
+    initFadeAnimation();
+
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: fadeAnimation,
+      builder: (context, _) {
+        return FadeTransition(
+          opacity: fadeAnimation,
+          child: SplashViewBody(),
+        );
+      },
+    );
+  }
+
+  void initFadeAnimation() {
+    animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 2500));
+    fadeAnimation = Tween<double>(begin: 0, end: 1).animate(animationController);
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(Duration(milliseconds: 2700), () {
+      Get.to(() => HomeView(), transition: Transition.fadeIn, duration: kTranstionDuration);
+    });
+  }
+}
