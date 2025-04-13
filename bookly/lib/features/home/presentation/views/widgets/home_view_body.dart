8589eb32b231426c51eaf07d585c20dec9d2ebb5
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-04-13 19:46:55 +0200

    add styles class

--- bookly/lib/features/home/presentation/views/widgets/home_view_body.dart
+++ bookly/lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/list_view_item_builder.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +8,24 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CustomAppBar(),
-        ListViewItemBuilder(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          ListViewItemBuilder(),
+          const SizedBox(
+            height: 50,
+          ),
+          Text(
+            "Best Seller",
+            style: Styles.titleMedium,
+          )
+        ],
+      ),
     );
   }
 }
