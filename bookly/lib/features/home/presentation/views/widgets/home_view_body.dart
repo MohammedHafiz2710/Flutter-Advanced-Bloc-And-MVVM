f25b8c75815f670dba40ab21d5b9c9b6038350f9
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-04-14 17:41:10 +0200

    create app styles

--- bookly/lib/features/home/presentation/views/widgets/home_view_body.dart
+++ bookly/lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/list_view_item_builder.dart';
@@ -22,7 +24,23 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             "Best Seller",
-            style: Styles.titleMedium,
+            style: Styles.textStyle18.copyWith(fontFamily: kGtSectraFine),
+          ),
+          SizedBox(
+            height: 125,
+            child: Row(
+              children: [
+                AspectRatio(
+                  aspectRatio: 1.3 / 2,
+                  child: Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: Colors.red, image: DecorationImage(image: AssetImage(AssetsData.jungleBook), fit: BoxFit.fill)),
+                  ),
+                ),
+                Column(
+                  children: [],
+                )
+              ],
+            ),
           )
         ],
       ),
