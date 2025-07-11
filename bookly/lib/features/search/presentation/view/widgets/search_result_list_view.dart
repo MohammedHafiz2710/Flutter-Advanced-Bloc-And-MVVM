427cfa8695bed15f92af6ddd5ea5170ac717a76c
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-07-11 15:12:35 +0300

    search view done

---
+++ bookly/lib/features/search/presentation/view/widgets/search_result_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:bookly/features/home/presentation/views/widgets/best_seller_item.dart';
+import 'package:flutter/material.dart';
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(childCount: 10, (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: const BestSellerItem(),
+        );
+      }),
+    );
+  }
+}
--- bookly/lib/features/search/presentation/view/widgets/search_view_body.dart
+++ bookly/lib/features/search/presentation/view/widgets/search_view_body.dart
@@ -1,4 +1,7 @@
+import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/search/presentation/view/widgets/custom_search_text_field.dart';
+import 'package:bookly/features/search/presentation/view/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -11,11 +14,19 @@ class SearchViewBody extends StatelessWidget {
     return SafeArea(
         child: Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          CustomSearchTextField()
-        ],
-      ),
+      child: CustomScrollView(physics: const BouncingScrollPhysics(), slivers: [
+        SliverToBoxAdapter(child: CustomSearchTextField()),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Text(
+              "Search Results",
+              style: Styles.textStyle18.copyWith(fontFamily: kGtSectraFine),
+            ),
+          ),
+        ),
+        SearchResultListView(),
+      ]),
     ));
   }
 }
