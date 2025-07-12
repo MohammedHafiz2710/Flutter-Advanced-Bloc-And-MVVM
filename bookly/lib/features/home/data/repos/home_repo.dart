0e0e4e0b3fb9f6aeec0c06917f4ebe6c779adc7d
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-07-12 13:30:42 +0300

    create home repo & failure file

---
+++ bookly/lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/features/home/data/models/book/book.model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure,List<BookModel>>> fetchFeatureBooks();
+}
