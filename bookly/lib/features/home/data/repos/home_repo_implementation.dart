a3615d9c9e345394a7dd83e976b8b6411e0f3b15
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-07-13 15:41:11 +0300

    add GetIt to our app

--- bookly/lib/features/home/data/repos/home_repo_implementation.dart
+++ bookly/lib/features/home/data/repos/home_repo_implementation.dart
@@ -8,7 +8,7 @@ import 'package:dio/dio.dart';
 class HomeRepoImplementation implements HomeRepo {
   final ApiService apiService;
 
-  HomeRepoImplementation({required this.apiService});
+  HomeRepoImplementation(this.apiService);
   @override
   Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
     try {
--- bookly/lib/main.dart
+++ bookly/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
+import 'package:bookly/features/home/data/repos/home_repo_implementation.dart';
+import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -12,14 +20,20 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => NewestBooksCubit(getIt.get<HomeRepoImplementation>())),
+        BlocProvider(create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImplementation>())),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
+        title: 'Bookly App',
       ),
-      title: 'Bookly App',
     );
   }
 }
