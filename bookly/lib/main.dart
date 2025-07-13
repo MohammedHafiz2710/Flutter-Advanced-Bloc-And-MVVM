450e9d7817f42eeab619e5ea576e066867c2fc22
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-07-13 15:43:09 +0300

    change name of setup -> setupServiceLocator

--- bookly/lib/main.dart
+++ bookly/lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:bookly/constants.dart';
-import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/service_locator.dart';
 import 'package:bookly/features/home/data/repos/home_repo_implementation.dart';
 import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
+
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
