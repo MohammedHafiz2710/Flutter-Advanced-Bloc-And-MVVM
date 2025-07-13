9463fc61c80df4be1fd6f5249595aea6ecd84e8c
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-07-13 15:09:41 +0300

    newest_books_cubit done

---
+++ bookly/lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,18 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/models/book/book.model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold(
+      (failure) => emit(NewestBooksFailure(errorMessage: failure.errorMessage)),
+      (books) => emit(NewestBooksSuccess(books: books)),
+    );
+  }
+}
