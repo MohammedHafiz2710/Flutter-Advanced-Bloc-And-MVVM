5ee277f07fe7cd955492c40e4b64291f2cd41999
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-07-13 14:58:39 +0300

    create FeaturedBooks Cubit

--- bookly/lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ bookly/lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,10 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/data/models/book/book.model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
-  
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeatureBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeatureBooks();
+    result.fold(
+      (failure) => emit(FeaturedBooksFailure(errorMessage: failure.errorMessage)),
+      (books) => emit(FeaturedBooksSuccess(books: books)),
+    );
+  }
 }
